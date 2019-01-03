9c096b071b6a8d308531d4c562fd68a3bfe49d1d
Benjamin Hofner <benjamin.hofner@pei.de>
2019-01-03 13:30:02 +0000

    version 1.0-4

--- inst/doc/papeR_with_latex.R
+++ inst/doc/papeR_with_latex.R
@@ -1,5 +1,5 @@
 ## ----include = FALSE-----------------------------------------------------
-library("papeR")
+library("knitr")
 opts_chunk$set(message = FALSE, warning = FALSE, echo = TRUE, results = 'asis')
 data(Orthodont, package = "nlme")
 
